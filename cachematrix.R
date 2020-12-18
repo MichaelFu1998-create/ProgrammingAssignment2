6831e28f400a76340928c006be960436d2c2ca1b
Michael <michaelfu1998@gmail.com>
2020-12-18 19:22:43 +1100

    V2_final ver

--- cachematrix.R
+++ cachematrix.R
@@ -5,10 +5,9 @@
 ## computing efforts by caching the computed data using "<<-"
 ## to assign a value to an object in an environment
 
-## Write a short comment describing this function
 ## in this function, we get/set the inverse using "<<-"
 ## to assign those values into an obj in the environment
-## (cach the data)
+## (cache the data)
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -22,11 +21,11 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+## in this function, we first check if we need to do the calculation
+## if so, calculate the inverse then cache it
+## if the data is already cached in the current environment
+## we do not have to compute again, hence, the efficiency is enhanced
 
-## Write a short comment describing this function
-## in this function, we calculate the inverse once then cache it
-## so we do not have to calculate it again when we need that value 
-## in the current environment, hence, the efficiency is enhanced
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
